16e1b052a56e7071607dafa6b796c0d3dcac9f09
Henry Solberg <milkshakeiii@gmail.com>
2015-12-04 20:08:33 -0800

    add unity stuff

---
+++ Assets/IceShape.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IceShape : MonoBehaviour
+{
+
+    public GameObject iceBlock;
+    public float blockSpacing = 4;
+    public Vector2[] blockPositions = new Vector2[3];
+    public float slideSpeed;
+
+    private int blocktype = -1;
+    private int column = -1;
+    private bool lockedIn = false;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        lockedIn = true;
+
+    }
+
+    public void SetColumn(int newColumn)
+    {
+        column = newColumn;
+    }
+
+    void Update()
+    {
+        if (!lockedIn)
+            gameObject.transform.position = gameObject.transform.position - new Vector3(0, Time.deltaTime * slideSpeed, 0);
+    }
+
+	public void InitializeIceShape (bool onLeftEdge, bool onRightEdge)
+    {
+        if (onLeftEdge && onRightEdge)
+            throw new UnityException("you can't be on both the left and right edge");
+        RandomizeBlockPositions(onLeftEdge, onRightEdge);
+        InstantiateBlocks();
+	}
+
+    private void RandomizeBlockPositions(bool onLeftEdge, bool onRightEdge)
+    {
+        blocktype = Random.Range(0, 6);
+        if (onLeftEdge) //3, 4, 5 are ok for left edge
+            blocktype = Random.Range(3, 6);
+        if (onRightEdge) //1, 2, 3 are ok for right edge
+            blocktype = Random.Range(1, 4);
+
+        blockPositions[0] = new Vector2(0, 0);
+
+        switch (blocktype)
+        {
+            case 0:
+                blockPositions[1] = new Vector2(1, 0);
+                blockPositions[2] = new Vector2(-1, 0);
+                break;
+            case 1:
+                blockPositions[1] = new Vector2(0, 1);
+                blockPositions[2] = new Vector2(-1, 0);
+                break;
+            case 2:
+                blockPositions[1] = new Vector2(0, -1);
+                blockPositions[2] = new Vector2(-1, 0);
+                break;
+            case 3:
+                blockPositions[1] = new Vector2(0, -1);
+                blockPositions[2] = new Vector2(0, 1);
+                break;
+            case 4:
+                blockPositions[1] = new Vector2(0, -1);
+                blockPositions[2] = new Vector2(1, 0);
+                break;
+            case 5:
+                blockPositions[1] = new Vector2(0, 1);
+                blockPositions[2] = new Vector2(1, 0);
+                break; 
+            default:
+                throw new UnityException("unknown blocktype");
+        }
+    }
+
+    private void InstantiateBlocks()
+    {
+        foreach(Vector2 blockPosition in blockPositions)
+        {
+            GameObject newBlock = Instantiate(iceBlock) as GameObject;
+            newBlock.transform.SetParent(gameObject.transform);
+            newBlock.transform.localPosition = new Vector3(blockPosition.x * blockSpacing, blockPosition.y * blockSpacing, 0);
+        }
+
+    }
+
+}
---
+++ Assets/IceShapeGenerator.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IceShapeGenerator : MonoBehaviour
+{
+    public GameObject iceShape;
+    public float frequency = 4;
+    public float spacing = 4;
+    public int columnCount = 8;
+
+    private float lastGeneration;
+    private int lastColumn = -2;
+    private int twoColumnsAgo = -2;
+
+   
+
+	void Update ()
+    {
+	    if (Time.time - lastGeneration > frequency)
+        {
+            DoGeneration();
+            lastGeneration = Time.time;
+        }
+	}
+    
+    private void DoGeneration()
+    {
+        GameObject newShape = Instantiate(iceShape) as GameObject;
+        IceShape newIceShape = newShape.GetComponent<IceShape>();
+
+        int column = -1;
+        System.Collections.Generic.List<int> okColumns = new System.Collections.Generic.List<int>();
+        for (int i = 0; i < columnCount; i++)
+            okColumns.Add(i);
+
+        for (int i = lastColumn - 1; i <= lastColumn + 1; i++)
+            if (okColumns.Contains(i))
+                okColumns.Remove(i);
+        for (int i = twoColumnsAgo - 1; i <= twoColumnsAgo + 1; i++)
+            if (okColumns.Contains(i))
+                okColumns.Remove(i);
+
+
+        //if (okColumns.Count == 0)
+        //{
+        //    column = -2;
+        //    twoColumnsAgo = lastColumn;
+        //    lastColumn = column;
+        //    Debug.Log("not enuogh room");
+        //    return;
+        //}
+
+
+        column = okColumns[Random.Range(0, okColumns.Count)];
+        twoColumnsAgo = lastColumn;
+        lastColumn = column;
+
+
+        if (column == 0)
+            newIceShape.InitializeIceShape(true, false);
+        else if (column == columnCount - 1)
+            newIceShape.InitializeIceShape(false, true);
+        else
+            newIceShape.InitializeIceShape(false, false);
+
+        newIceShape.SetColumn(column);
+
+        newShape.transform.position = gameObject.transform.position + new Vector3(column * spacing, 0, 0);
+    }
+}
